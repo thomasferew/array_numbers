452819d4f9508b1ee92d70aa7289cb4b059dc458
thomasferew <58610700+thomasferew@users.noreply.github.com>
2020-02-27 14:03:19 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+// create an empty array of numbers
+
+let arrayLoop: number[] = []
+
+for (let index = 0; index < 4; index++) {
+    arrayLoop[index] = index *2
+
+    game.splash(i + "multiplied tiimes two is:" + arrayLoop[i])
+}
+    
+
